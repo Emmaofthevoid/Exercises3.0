999d5e811a69e7c6dc9926183b60453529c2c548
emmaofthevoid <tehurapardo@outlook.es>
2022-03-21 22:31:49 +0100

    sfgdf

---
+++ src/com/company/NarcissisticNumbers.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class NarcissisticNumbers {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isNarcissisticNumber (int number) {
+        
+        return true;
+    }
+}
---
+++ src/com/company/Straßenbahn3.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Straßenbahn3 {
+    public static void main(String[] args) {
+        int haltestelle = 1;
+
+            System.out.println(welcheHaltestelle(haltestelle));
+
+
+
+    }
+
+    public static String welcheHaltestelle(int haltestelle) {
+        String str = " ";
+        for (int i = haltestelle; haltestelle <= 6; haltestelle++) {
+
+            if (haltestelle == 1) {
+                str = "von die haltestelle " + haltestelle + " sind es noch 20mn bis zum Ende";
+                return str;
+            } else if (haltestelle == 2) {
+                str = "von die haltestelle" + haltestelle + "sind es noch 16mn bis zum Ende";
+                return str;
+            } else if (haltestelle == 3) {
+                str = "von die haltestelle" + haltestelle + "sind es noch 15mn bis zum Ende";
+                return str;
+            } else if (haltestelle == 4) {
+                str = "von die haltestelle" + haltestelle + "sind es noch 10mn bis zum Ende";
+                return str;
+            } else if (haltestelle == 5) {
+                str = "von die haltestelle" + haltestelle + "sind es noch 2mn bis zum Ende";
+                return str;
+            } else if (haltestelle == 6) {
+                str = "please get tf out of my bus";
+                return str;
+            }
+        }
+        return str;
+    }
+}
+
+
